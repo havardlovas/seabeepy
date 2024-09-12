f92dde5fae06f26e67a2431fe5f7d78216428f15
havardlovas <havard.s.lovas@ntnu.no>
2024-09-12 09:45:24 +0200

    Added support for processing Resonon data

--- notebooks/process_hyperspectral.py
+++ notebooks/process_hyperspectral.py
@@ -13,9 +13,10 @@ import importlib
 
 
 from gref4hsi.scripts import georeference, orthorectification, coregistration
-from gref4hsi.utils import parsing_utils, specim_parsing_utils
+from gref4hsi.utils import parsing_utils, specim_parsing_utils, resonon_parsing_utils
 from gref4hsi.utils import visualize
 from gref4hsi.utils.config_utils import prepend_data_dir_to_relative_paths, customize_config
+
 
 #importlib.reload(gref4hsi)
 importlib.reload(georeference)
@@ -23,6 +24,9 @@ importlib.reload(orthorectification)
 importlib.reload(coregistration)
 
 importlib.reload(specim_parsing_utils)
+importlib.reload(resonon_parsing_utils)
+
+from gref4hsi.utils.resonon_parsing_utils import ResononImage
 
 import numpy as np
 
@@ -34,14 +38,13 @@ This script is meant to be used for testing the processing pipeline of airborne 
 
 
 
-def main(config_yaml, specim_mission_folder, geoid_path, config_template_path, lab_calibration_path, fast_mode = False):
+def main(config_yaml, specim_mission_folder, geoid_path, config_template_path, lab_calibration_path, device, processing_lvl, fast_mode = False):
     # Read flight-specific yaml file
     with open(config_yaml, 'r') as file:  
         config_data = yaml.safe_load(file)
     
     
     # assigning the arguments to variables for simple backwards compatibility
-    SPECIM_MISSION_FOLDER = specim_mission_folder
     EPSG_CODE = config_data['mission_epsg']
     RESOLUTION_ORTHOMOSAIC = config_data['resolution_orthomosaic']
     CALIBRATION_DIRECTORY = lab_calibration_path
@@ -63,7 +66,6 @@ def main(config_yaml, specim_mission_folder, geoid_path, config_template_path, l
             DEM_PATH = os.path.join(dem_fold, files[0])
             #print(f"The file '{DEM_PATH}' is used as terrain.")
             TERRAIN_TYPE = "dem_file"
-    
     
     # Do coregistration if there is an orthomosaic to compare under "orthomosaic"
     #do_coreg = True
@@ -98,9 +100,9 @@ def main(config_yaml, specim_mission_folder, geoid_path, config_template_path, l
 
     config_specim_preprocess = SettingsPreprocess(dtype_datacube = np.float32, # The data type for the datacube
                                 lines_per_chunk= 2000,  # Raw datacube is chunked into this many lines. GB_per_chunk = lines_per_chunk*n_pixels*n_bands*4 bytes
-                                specim_raw_mission_dir = SPECIM_MISSION_FOLDER, # Folder containing several mission
+                                specim_raw_mission_dir = specim_mission_folder, # Folder containing several mission
                                 cal_dir = CALIBRATION_DIRECTORY,  # Calibration directory holding all calibrations at all binning levels
-                                reformatted_missions_dir = os.path.join(SPECIM_MISSION_FOLDER, 'processed'), # The fill value for empty cells (select values not occcuring in cube or ancillary data)
+                                reformatted_missions_dir = os.path.join(specim_mission_folder, 'processed'), # The fill value for empty cells (select values not occcuring in cube or ancillary data)
                                 rotation_matrix_hsi_to_body = np.array([[0, 1, 0],
                                                                         [-1, 0, 0],
                                                                         [0, 0, 1]]), # Rotation matrix R rotating so that vec_body = R*vec_hsi.
@@ -112,17 +114,16 @@ def main(config_yaml, specim_mission_folder, geoid_path, config_template_path, l
 
     # Where to place the config
     DATA_DIR = config_specim_preprocess.reformatted_missions_dir
-    config_file_mission = os.path.join(DATA_DIR, 'configuration.ini')
+    config_file = os.path.join(DATA_DIR, 'configuration.ini')
 
 
-    # Set the data directory for the mission, and create empty folder structure
-    prepend_data_dir_to_relative_paths(config_path=config_template_path, DATA_DIR=DATA_DIR)
+    
 
     # Non-default settings
     custom_config = {'General':
                         {'mission_dir': DATA_DIR,
                         'model_export_type': TERRAIN_TYPE, # Ray trace onto geoid
-                        'max_ray_length': 150}, # Max distance in meters from spectral imager to seafloor. Specim does not fly higher
+                        'max_ray_length': 300}, # Max distance in meters from spectral imager to seafloor. Specim does not fly higher
 
                     'Coordinate Reference Systems':
                         {'proj_epsg' : EPSG_CODE, # The projected CRS UTM 32, common on mainland norway
@@ -135,11 +136,6 @@ def main(config_yaml, specim_mission_folder, geoid_path, config_template_path, l
                          'resample_ancillary': True,
                         'resolutionhyperspectralmosaic': RESOLUTION_ORTHOMOSAIC, # Resolution in m
                         'raster_transform_method': 'north_east'}, # North-east oriented rasters.
-                    
-                    'HDF.raw_nav': {
-                        'rotation_reference_type' : 'eul_ZYX', # The vehicle orientations are given in Yaw, Pitch, Roll from the NAV system
-                        'is_global_rot' : False, # The vehicles orientations from NAV system are Yaw, Pitch, Roll
-                        'eul_is_degrees' : True}, # And given in degrees
                     'Absolute Paths': {
                         'geoid_path' : GEOID_PATH,
                         'orthomosaic_reference_folder' : os.path.join(specim_mission_folder, "orthomosaic"),
@@ -224,32 +220,50 @@ def main(config_yaml, specim_mission_folder, geoid_path, config_template_path, l
     
     # 
     if fast_mode:
-        custom_config['Orthorectification']['resample_rgb_only'] = False
-        custom_config['Orthorectification']['resolutionhyperspectralmosaic'] = 1
+        custom_config['Orthorectification']['resample_rgb_only'] = True
+        custom_config['Orthorectification']['resolutionhyperspectralmosaic'] = 0.05
 
 
-    # Customizes the config file according to settings
-    customize_config(config_path=config_file_mission, dict_custom=custom_config)
+    # Customizes the config file according to settings if it does not exist
+    
+    if not os.path.exists(config_file):
+        print('Generating config file from template')
+        # Set the data directory for the mission, and create empty folder structure
+        prepend_data_dir_to_relative_paths(config_path=config_template_path, DATA_DIR=DATA_DIR)
+        customize_config(config_path=config_file, dict_custom=custom_config)
+    else:
+        print('Using existing config file under /processed folder')
 
 
     config = configparser.ConfigParser()
-    config.read(config_file_mission)
+    config.read(config_file)
 
     # This function parses raw specim data including (spectral, radiometric, geometric) calibrations and nav data
     # into an h5 file. The nav data is written to "raw/nav/" subfolders, whereas hyperspectral data and calibration data 
     # written to "processed/hyperspectral/" and "processed/calibration/" subfolders
     
     
-    if len(os.listdir(config['Absolute Paths']['h5_folder'])) == 0:
-        print(len(os.listdir(config['Absolute Paths']['h5_folder'])))
+    
+        
+    
+    if device == 'specim':
         specim_parsing_utils.main(config=config,
                               config_specim=config_specim_preprocess)
-    
+    elif device == 'resonon':
+        # Parse the resonon image data and georeference
+        res_img = ResononImage(config, 
+                               config_file,
+                               config_specim_preprocess,
+                               specim_mission_folder, 
+                               processing_lvl, 
+                               afov_deg=float(config['General']['afov']))
+        res_img.format_2_gref4hsi()
+            
     # Time interpolates and reformats the pose (of the vehicle body) to "processed/nav/" folder.
-    parsing_utils.export_pose(config_file_mission)
+    parsing_utils.export_pose(config_file)
     
     # Formats model to triangular mesh and an earth centered earth fixed / geocentric coordinate system
-    parsing_utils.export_model(config_file_mission)
+    parsing_utils.export_model(config_file)
 
     # Commenting out the georeference step is fine if it has been done
 
@@ -258,10 +272,10 @@ def main(config_yaml, specim_mission_folder, geoid_path, config_template_path, l
     #visualize.show_mesh_camera(config, show_mesh = True, show_pose = True, ref_frame='ENU')
 
     # Step 1: Direct georeferencing
-    georeference.main(config_file_mission)
+    georeference.main(config_file)
 
-    # Step 2: Orthorectify the direct georeferenced data (incl metadata)
-    orthorectification.main(config_file_mission)
+    # Step 2: Orthorectify the direct georeferenced data (incl metadata) i.e. resampling
+    orthorectification.main(config_file)
     
 
 
@@ -271,29 +285,29 @@ def main(config_yaml, specim_mission_folder, geoid_path, config_template_path, l
         # Coregistration requires that Step 1 and 2 were performed and that resample anc = True
         # Match RGB composite to reference, find features and following data, ground control point (gcp) list, for each feature pair:
         # reference point 3D (from reference), position/orientation of vehicle (using resampled time) and pixel coordinate (using resampled pixel coordinate)
-        coregistration.main(config_file_mission, mode='compare', is_calibrated = False)
+        coregistration.main(config_file, mode='compare', is_calibrated = False)
 
         # The gcp list allows reprojecting reference points and evaluate the reprojection error,
         # which is used to optimize static geometric parameters (e.g. boresight, camera model...) or dynamic geometric parameters (time-varying nav errors).
         # Settings are currently in coregistration script
-        coregistration.main(config_file_mission, mode='calibrate', is_calibrated = False, coreg_dict = coreg_dict)
+        coregistration.main(config_file, mode='calibrate', is_calibrated = False, coreg_dict = coreg_dict)
 
         # Resample full datacube
         custom_config['Orthorectification']['resample_rgb_only'] = False
-        customize_config(config_path=config_file_mission, dict_custom=custom_config)
+        customize_config(config_path=config_file, dict_custom=custom_config)
         
         # Re-georeference with coregistred parameters
-        georeference.main(config_file_mission, use_coreg_param=True)
+        georeference.main(config_file, use_coreg_param=True)
             
 
         # Coregister this stuff
-        orthorectification.main(config_file_mission)
+        orthorectification.main(config_file)
 
         # Second round of comparison
-        coregistration.main(config_file_mission, mode='compare', is_calibrated = True)
+        coregistration.main(config_file, mode='compare', is_calibrated = True)
 
         # Check bulk
-        coregistration.main(config_file_mission, mode='calibrate', is_calibrated = True, coreg_dict = coreg_dict)
+        coregistration.main(config_file, mode='calibrate', is_calibrated = True, coreg_dict = coreg_dict)
     
     
 
